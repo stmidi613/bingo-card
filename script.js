3bccd38b14253837651825df4d97815ae764cdde
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2021-10-09 15:06:11 +0900

    I want to upload this to github.

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("hello, World")
+
+let column = 4;
+let rows = 4;
+let arr = [[1]];
+
+const ARRAY = ["A1", "A2", "A3", "A4", "A5"];
+
+function shuffleArray(ARRAY){
+    let shuffledArr = [];
+    function randInd(){
+         return Math.floor(Math.random() * ARRAY.length);
+    }
+    let count = ARRAY.length;
+    while(0 !== count){
+        let randomInd = randInd();
+        shuffledArr.push(ARRAY.splice(randomInd, 1)[0]);
+        count--;
+    }
+    return shuffledArr;
+}
+
+console.log(shuffleArray(ARRAY))
+
+
+window.onload = function(){
+
+    arr.map(array => {
+document.getElementById("card").insertAdjacentHTML("beforeend", 
+    `<tr>${array.map(item =>
+        `<td>${item}</td>`.repeat(column))}
+    </tr>`.repeat(rows)
+    )
+})
+
+}
